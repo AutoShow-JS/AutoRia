f60241912a16108390e632605640902a4c01f482
Witcher-MTM <phantomit228@gmail.com>
2022-12-21 22:05:21 +0200

    add function Init
    Init - create cards with autos from array

--- script.js
+++ script.js
@@ -2,14 +2,117 @@ var apiKey = "kxBcXJKhCrmQHLck7hkmT5HuKjPReDajM9h5yTJx";
 var user_id = '9684073';
 var idArray = [];
 var carsForApi = [];
-var cars=[];
+
+const gearBoxType = {
+  Auto:1,
+  Manual:2
+}
+const EngineType = {
+  Fuel:1,
+  Gas:2,
+  Electro:3,
+  Diezel:4,
+  Symbiot:5
+}
+var cars=[
+  {manufacture:"Honda",
+  model:"eNS1 Dynamic" , 
+  price:"25000" , 
+  gearBox:2 , 
+  engine:1 ,
+  img:"source/img/mercedes-amg-gt.png"},
+
+  {manufacture:"Mercedes",
+  model:"AMG GT" , 
+  price:"25000" , 
+  gearBox:1 , 
+  engine:1 ,
+  img:"source/img/mercedes-amg-gt.png"},
+
+  {manufacture:"Mercedes",
+  model:"AMG GT" , 
+  price:"25000" , 
+  gearBox:1 , 
+  engine:1 ,
+  img:"source/img/mercedes-amg-gt.png"},
+
+  {manufacture:"Mercedes",
+  model:"AMG GT" , 
+  price:"25000" , 
+  gearBox:1 , 
+  engine:1 ,
+  img:"source/img/mercedes-amg-gt.png"},
+
+  {manufacture:"Mercedes",
+  model:"AMG GT" , 
+  price:"25000" , 
+  gearBox:1 , 
+  engine:1 ,
+  img:"source/img/mercedes-amg-gt.png"}
+];
 window.onload = async () => {
   SetMaxLabel();
-
+    Init();
    //await RequestCarsId();
    //await RequestCarsById();
 };
 //Added cards 
+function Init(){
+  cars.forEach(car => {
+      var div_container = document.createElement('div');
+      var div_card = document.createElement('div');
+      var div_imgBx = document.createElement('div');
+      var img = document.createElement('img');
+      var div_contentBx = document.createElement('div');
+      var h1_car_manufacture = document.createElement('h1');
+      var h2_model = document.createElement('h2');
+      var div_size = document.createElement('div');
+      var h3_label_price = document.createElement('h3');
+      var span_price = document.createElement('span');
+      var div_color = document.createElement('div');
+      var h3_label_gearbox = document.createElement('h3');
+      var span_typegearbox = document.createElement('span');
+      var a = document.createElement('a');
+
+      document.getElementById('grid').appendChild(div_container);
+      div_container.className = "container";
+      div_container.appendChild(div_card);
+      div_card.className = "card";
+      div_card.style.content = car.manufacture;
+      div_card.appendChild(div_imgBx);
+      div_imgBx.className = "imgBx";
+      div_imgBx.appendChild(img);
+      img.src = car.img;
+      div_card.appendChild(div_contentBx);
+      div_contentBx.className = "contentBx"
+      div_contentBx.appendChild(h1_car_manufacture);
+      h1_car_manufacture.className = "car_manufacture";
+      h1_car_manufacture.innerText = car.manufacture;
+      h1_car_manufacture.style.display = "none";
+      div_contentBx.appendChild(h2_model);
+      h2_model.className = "model";
+      h2_model.innerText = car.model;
+      div_contentBx.appendChild(div_size);
+      div_size.className = "size";
+      div_size.appendChild(h3_label_price);
+      h3_label_price.innerText = "Price: ";
+      h3_label_price.className = "price_h3";
+      div_size.appendChild(span_price);
+      span_price.className = "price";
+      span_price.innerText = car.price;
+      div_contentBx.appendChild(div_color);
+      div_color.className = "color";
+      div_color.appendChild(h3_label_gearbox);
+      h3_label_gearbox.className = "label_gearbox_h3";
+      h3_label_gearbox.innerText = "Gearbox: ";
+      div_color.appendChild(span_typegearbox);
+      span_typegearbox.className = "typegearbox";
+      span_typegearbox.innerText = car.gearBox == 1?"Auto":"Manual";
+      div_contentBx.appendChild(a);
+      a.href = "#";
+      a.innerText = "Buy now";
+  }); 
+}
 //получаем id машин
  async function RequestCarsId() {
    let searchUrl = `https://developers.ria.com/auto/search?api_key=${apiKey}&category_id=1&marka_id=28&with_photo=1`;
